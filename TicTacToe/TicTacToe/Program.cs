00204429edd4e6e9fb9700f4b783bc08b1babae6
VikasBenki <vikasmsbenki@gmail.com>
2022-03-07 09:38:04 +0530

    [VikasBenki] adding the welcome message

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello welcome to TicTactoe Game");
+            Console.ReadLine();
+        }
+    }
+}
